de52cf82c40d5bc1f624bb52891fd7e3faaae02e
Alvaro <aduyaguari@gmail.com>
2024-04-03 14:56:32 -0500

    vectorizador

--- testVectorizar.py
+++ testVectorizar.py
@@ -5,5 +5,6 @@ if __name__ == '__main__':
 
     vectorizador =  Vectorizador("last_model.pth",'sentence-transformers/paraphrase-multilingual-mpnet-base-v2' )
     vect = vectorizador("Hola mundo")
+    print ("paso")
     print(vect)
     
