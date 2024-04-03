46590628f9eaf7afd2a86f6482d9944eb6a97701
Alvaro <aduyaguari@gmail.com>
2024-04-03 15:05:06 -0500

    litle hcange

--- testVectorizar.py
+++ testVectorizar.py
@@ -3,7 +3,7 @@ from Servicios.VectorizadorServicio import Vectorizador
 
 if __name__ == '__main__':
 
-    vectorizador =  Vectorizador("last_model.pth",'sentence-transformers/paraphrase-multilingual-mpnet-base-v2' )
+    vectorizador =  Vectorizador("../output/last_model.pth",'sentence-transformers/paraphrase-multilingual-mpnet-base-v2' )
     vect = vectorizador("Hola mundo")
     print ("que paso")
     print(vect)
